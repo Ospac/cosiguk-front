96760b906a22d6a087c65b63b110ce1ea82a1e9f
geha <ospac111@gmail.com>
2021-11-19 02:15:49 +0900

    post-view comment

--- src/routes/PostView.js
+++ src/routes/PostView.js
@@ -4,7 +4,7 @@ import {useParams} from 'react-router-dom'
 import axios from 'axios';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
-import { Tooltip } from '@mui/material';
+import { dividerClasses, Tooltip } from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import InsertCommentIcon from '@mui/icons-material/InsertComment';
 
@@ -16,7 +16,7 @@ function PostView(){
             if(json.error !== undefined) alert(json.error);
             else {
                 alert("추천 하였습니다.");
-                window.location.replace("/board");
+                // window.location.replace("/board");
             }
         })
         .catch((error) => {console.log(error)});
@@ -31,29 +31,60 @@ function PostView(){
         .catch((error) => {console.log(error)});
     }
 
+    const onCRecommend = async(e) => {
+        fetch(`/api/chat/${e.currentTarget.value}/recommend`)
+        .then((res) => res.json())
+        .then((json) => {
+            if(json.error !== undefined) alert(json.error);
+            else {
+                alert("추천 하였습니다.");
+            }
+        })
+        .catch((error) => {console.log(error)});
+    }
+    const onCDeprecate = async(e) => {
+        fetch(`/api/chat/${e.currentTarget.value}/deprecate`)
+        .then((res) => res.json())
+        .then((json) => {
+            if(json.error !== undefined) alert(json.error)
+            else alert("비 추천 하였습니다.");
+        })
+        .catch((error) => {console.log(error)});
+    }
+
     const params= useParams();
     const postId = params.id;
-    const getPostView = async() => {
+    const getPostData = async() => {
         const res = await axios.get(`/api/board/${postId}`)
         return res;
     }
-    const [state, refetch] = UseAsync(getPostView, []);
-    const {loading, data, error} = state;
-    if(loading) return <div className={styles.loader}>Loading</div>;
-    if(error) return <div>error page</div>;
-    if(!data) return <div>loading</div>
+    const getCommentData = async() => {
+        const res = await axios.get(`/api/chat/${postId}/chatList`)
+        return res;
+    }
+    const [postState, postRefetch] = UseAsync(getPostData, []);
+    const [commentState, commentRefetch] = UseAsync(getCommentData, []);
+    const {loading : postLoading, data : postData, error : postError} = postState;
+    const {loading : commentLoading, data : commentData, error : commentError} = commentState;
 
-    const postData = data.data.result_data.account
+    if(postLoading || commentLoading) return <div className={styles.loader}>Loading</div>;
+    if(postError || commentError) return <div>error page</div>;
+    if(!postData || !commentData) return <div>loading</div>
+
+
+    const post = postData.data.result_data.account;
+    const comments = commentData.data.result_data.data;
     return(
+        <div className={styles.postViewContainer}>
         <div className={styles.postContainer}>
             <div className={styles.postHeader}>
-                    <div className={styles.postNickname}>{postData.nickname}</div>
-                    <div className={styles.postDate}>{postData.createDate}</div>
-                    <div className={styles.postViews}>{postData.hit} views</div>
+                    <div className={styles.postNickname}>{post.nickname}</div>
+                    <div className={styles.postDate}>{post.createDate}</div>
+                    <div className={styles.postViews}>{post.hit} views</div>
             </div>
             <div className={styles.postBody}>
-                <div className={styles.postTitle}>{postData.title}</div>
-                <div className={styles.postContent}>{postData.content}</div>
+                <div className={styles.postTitle}>{post.title}</div>
+                <div className={styles.postContent}>{post.content}</div>
             </div>
             <div className={styles.postFooter}>
                 <div className={styles.voting}>
@@ -66,14 +97,45 @@ function PostView(){
                 </div>
                 <div className={styles.recommendStats}>
                     <Tooltip title="추천 수" arrow><KeyboardArrowUpIcon style={{ fill: 'rgba(0,0,0,0.45)'}}fontSize="small"/></Tooltip>
-                    <div className={styles.recommendCnt}>{postData.recommend} </div>
+                    <div className={styles.recommendCnt}>{post.recommend} </div>
                 </div>
             </div>
-            <div className={styles.commentContainer}>
-                <div className={styles.commentCnt}></div>
-                <div></div>
-            </div>
         </div>
+        <div className={styles.commentCnt}>{comments.length} comments</div>
+        <div className={styles.commentView}>
+            {comments.map((com) => 
+                <div className={styles.commentContainer}>
+                    <div className={styles.commentHeader}>
+                        <div className={styles.commentNickname}>{com.nickname}</div>
+                        <div className={styles.commentDate}>{com.createdDate}</div>
+                    </div>
+                    <div className={styles.commentContents}>
+                        {com.content}
+                    </div>
+                    <div className={styles.commentFooter}>
+                        <div className={styles.voting}>
+                            <button value={com.id} className={styles.recommend} onClick={onCRecommend}>
+                                <Tooltip title="추천" arrow>{<ThumbUpAltIcon style={{ fill: 'rgba(0,0,0,0.45)' }}fontSize="10px" />}</Tooltip>
+                            </button>
+                            <button value={com.id} className={styles.deprecate} onClick={onCDeprecate}>
+                                <Tooltip title="비추천" arrow>{<ThumbDownAltIcon style={{ fill: 'rgba(0,0,0,0.45)' }} fontSize="10px"/>}</Tooltip>
+                            </button>
+                        </div>
+                        <div className={styles.recommendStats}>
+                            <Tooltip title="추천 수" arrow><KeyboardArrowUpIcon style={{ fill: 'rgba(0,0,0,0.45)'}}fontSize="small"/></Tooltip>
+                            <div className={styles.recommendCnt}>{com.recommend} </div>
+                        </div>
+                    </div>
+                    <div className={styles.commentWriteContainer}>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            )}
+            
+        </div>
+        </div>
+        
     )
 }
 export default PostView;
